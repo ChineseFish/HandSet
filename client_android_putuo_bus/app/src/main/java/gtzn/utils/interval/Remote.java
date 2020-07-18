f7fdc3d9a2affbbe579fe4e766d5ed4156cb9b74
walker <565828928@qq.com>
2020-07-18 19:59:01 +0800

    if sleep is interupt , kill app

--- client_android_putuo_bus/app/src/main/java/gtzn/utils/interval/Remote.java
+++ client_android_putuo_bus/app/src/main/java/gtzn/utils/interval/Remote.java
@@ -1,14 +1,17 @@
 package gtzn.utils.interval;
 
+import android.app.AlertDialog;
 import android.app.KeyguardManager;
 import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.PowerManager;
 
 import com.tongda.putuoshanlvyoubashi.MainActivity;
 import com.tongda.putuoshanlvyoubashi.MyApplication;
+import com.tongda.putuoshanlvyoubashi.R;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -58,106 +61,114 @@ public class Remote extends BroadcastReceiver {
         //
         final boolean[] ifKeepRunning = {true};
 
+        /**
+         * begin to fetch data
+         */
+        Thread thread = new Thread() {
+            @Override
+            @PrintTimeElapseAnnotation("Remote fetchPayInfo")
+            public void run() {
+                //
+                URL url;
+                HttpURLConnection connection = null;
+
+                //
+                String index = Db.getBusIdentifierIndex(Interval.identifier);
+
+                //
+                try {
+
+                    String requestUrl = remoteUrl + "/ashx/GetPlayVoice.ashx?busIdentifier=" + Interval.identifier + "&index=" + index;
+
+                    // test
+                    LogUtils.d("fetchPayInfo requestUrl", requestUrl);
+
+                    //
+                    url = new URL(requestUrl);
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(2000);
+                    connection.setReadTimeout(2000);
+
+                    //
+                    LogUtils.d("fetchPayInfo", "begin to fetch data");
+
+                    //
+                    InputStream in = connection.getInputStream();
+
+                    // read response data
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+
+                    //
+                    LogUtils.d("fetchPayInfo response", response.toString());
+
+                    // translate to JSON format
+                    JSONArray speechTextList = new JSONArray(response.toString());
+                    if (speechTextList.length() <= 0) {
+                        return;
+                    }
+
+                    /**
+                     * alarm begin
+                     */
+                    // fetch speech text
+                    String speechText = "";
+                    for (int i = 0; i < speechTextList.length(); i++) {
+                        speechText += speechTextList.getJSONObject(i).getString("text");
+                    }
+
+                    // fetch new index
+                    int maxIndex = speechTextList.length() - 1;
+                    String newIndex = speechTextList.getJSONObject(maxIndex).getString("index");
+
+                    /**
+                     * speech
+                     */
+
+                    tts.textToSpeech(speechText);
+
+                    // update index
+                    Db.writeBusIdentifierIndex(Interval.identifier, newIndex);
+                } catch (MalformedURLException e) {
+                    LogUtils.e("fetchPayInfo throw exception", e.toString());
+                } catch (IOException | JSONException e) {
+                    LogUtils.e("fetchPayInfo throw exception", e.toString());
+                } catch (Exception e) {
+                    LogUtils.e("fetchPayInfo throw exception", e.toString());
+                } finally {
+                    if (connection != null) {
+                        connection.disconnect();
+                    }
+
+                    // jump to scan service
+                    Intent i = new Intent(context, ScanService.class);
+                    context.startService(i);
+
+                    //
+                    ifKeepRunning[0] = false;
+                }
+            }
+        };
+        thread.start();
+
+        /**
+         * wait util fetch data thread is over
+         */
         while(ifKeepRunning[0])
         {
-            Thread thread = new Thread() {
-                @Override
-                @PrintTimeElapseAnnotation("Remote fetchPayInfo")
-                public void run() {
-                    //
-                    URL url;
-                    HttpURLConnection connection = null;
-
-                    //
-                    String index = Db.getBusIdentifierIndex(Interval.identifier);
-
-                    //
-                    try {
-
-                        String requestUrl = remoteUrl + "/ashx/GetPlayVoice.ashx?busIdentifier=" + Interval.identifier + "&index=" + index;
-
-                        // test
-                        LogUtils.d("fetchPayInfo requestUrl", requestUrl);
-
-                        //
-                        url = new URL(requestUrl);
-                        connection = (HttpURLConnection) url.openConnection();
-                        connection.setRequestMethod("GET");
-                        connection.setConnectTimeout(2000);
-                        connection.setReadTimeout(2000);
-
-                        //
-                        LogUtils.d("fetchPayInfo", "begin to fetch data");
-
-                        //
-                        InputStream in = connection.getInputStream();
-
-                        // read response data
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-                        StringBuilder response = new StringBuilder();
-                        String line;
-                        while ((line = reader.readLine()) != null) {
-                            response.append(line);
-                        }
-
-                        //
-                        LogUtils.d("fetchPayInfo response", response.toString());
-
-                        // translate to JSON format
-                        JSONArray speechTextList = new JSONArray(response.toString());
-                        if (speechTextList.length() <= 0) {
-                            return;
-                        }
-
-                        /**
-                         * alarm begin
-                         */
-                        // fetch speech text
-                        String speechText = "";
-                        for (int i = 0; i < speechTextList.length(); i++) {
-                            speechText += speechTextList.getJSONObject(i).getString("text");
-                        }
-
-                        // fetch new index
-                        int maxIndex = speechTextList.length() - 1;
-                        String newIndex = speechTextList.getJSONObject(maxIndex).getString("index");
-
-                        /**
-                         * speech
-                         */
-
-                        tts.textToSpeech(speechText);
-
-                        // update index
-                        Db.writeBusIdentifierIndex(Interval.identifier, newIndex);
-                    } catch (MalformedURLException e) {
-                        LogUtils.e("fetchPayInfo throw exception", e.toString());
-                    } catch (IOException | JSONException e) {
-                        LogUtils.e("fetchPayInfo throw exception", e.toString());
-                    } catch (Exception e) {
-                        LogUtils.e("fetchPayInfo throw exception", e.toString());
-                    } finally {
-                        if (connection != null) {
-                            connection.disconnect();
-                        }
-
-                        // jump to scan service
-                        Intent i = new Intent(context, ScanService.class);
-                        context.startService(i);
-
-                        //
-                        ifKeepRunning[0] = false;
-                    }
-                }
-            };
-
-            thread.start();
-
-            //
             try {
                 Thread.sleep(250);
             } catch (InterruptedException e) {
+                //
                 LogUtils.d("onReceive Thread.sleep throw exception" , e.toString());
+
+                //
+                terminateApp(e.toString());
             }
         }
 
@@ -166,5 +177,38 @@ public class Remote extends BroadcastReceiver {
 
         //
         LogUtils.d("onReceive", "onReceive end");
+    }
+
+    private void terminateApp(final String err){
+        /* @setIcon 设置对话框图标
+         * @setTitle 设置对话框标题
+         * @setMessage 设置对话框消息提示
+         * setXXX方法返回Dialog对象，因此可以链式设置属性
+         */
+        final AlertDialog.Builder normalDialog =
+            new AlertDialog.Builder(MainActivity.getMainActivity());
+        normalDialog.setIcon(R.mipmap.ic_launcher);
+        normalDialog.setTitle("定时器出现问题，请点击确认按钮关闭app，然乎重启启动");
+        normalDialog.setMessage(err);
+        normalDialog.setPositiveButton("终止app",
+            new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // 先让app进入后台
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                MainActivity.getMainActivity().startActivity(intent);
+
+                // 调用系统API结束进程
+                android.os.Process.killProcess(android.os.Process.myPid());
+
+                // 结束整个虚拟机进程，注意如果在manifest里用android:process给app指定了不止一个进程，则只会结束当前进程
+                System.exit(0);
+            }
+        });
+        // 显示
+        normalDialog.show();
     }
 }
